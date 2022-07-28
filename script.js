776c9743a96c1b86e3756071f56bb0d86ee009ff
Sturgart <wesleyicaro4@gmail.com>
2022-07-27 22:12:50 -0300

    Primeiro commit.

---
+++ script.js
@@ -0,0 +1,81 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordConfirmation = document.getElementById('password-confirmation')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  checkInputs()
+})
+
+function checkInputs() {
+  const usernameValue = username.value
+  const emailValue = email.value
+  const passwordValue = password.value
+  const passwordConfirmationValue = passwordConfirmation.value
+
+  if (usernameValue === '') {
+    setErrorFor(username, 'O nome de usuário é obrigatório.')
+  } else {
+    setSucessFor(username)
+  }
+
+  if (emailValue === '') {
+    setErrorFor(email, 'O email é obrigatório.')
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, 'Por favor, insira um email válido.')
+  } else {
+    setSucessFor(email)
+  }
+
+  if (passwordValue === '') {
+    setErrorFor(password, 'A senha é obrigatória')
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, 'A senha precisa ter no minímo 7 caracteres')
+  } else {
+    setSucessFor(password)
+  }
+
+  if (passwordConfirmationValue === '') {
+    setErrorFor(passwordConfirmation, 'É obrigatório confirmar a senha')
+  } else if (passwordConfirmationValue !== passwordValue) {
+    setErrorFor(passwordConfirmation, 'As senhas devem ser iguais')
+  } else {
+    setSucessFor(passwordConfirmation)
+  }
+
+  const formControls = form.querySelectorAll('.form-control')
+
+  formIsValid = [...formControls].every(formControl => {
+    return formControl.className === 'form-control sucess'
+  })
+
+  if (formIsValid) {
+    console.log('O cadastro está totalmente concluído!')
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement
+  const small = formControl.querySelector('small')
+
+  // Adicionar a mensagem de erro
+  small.innerText = message
+
+  // Adcionar a classe de erro
+  formControl.className = 'form-control error'
+}
+
+function setSucessFor(input) {
+  const formControl = input.parentElement
+  // Adicionar a classe de sucesso
+  formControl.className = 'form-control sucess'
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  )
+}
